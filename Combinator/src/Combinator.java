f6cdfe8082c8cf4aa2e0fd169da5d73c1c7a2426
Medard Rebero <rmedard5@gmail.com>
2014-07-18 16:10:03 +0200

    Combinations and permutations first tests

--- Combinator/src/Combinator.java
+++ Combinator/src/Combinator.java
@@ -25,7 +25,7 @@ class Combinator {
 		return combinations;
 	}
 
-	// Permutations with repetition
+	// Permutations with repetition (operators: count = 4)
 	public Map<Integer, List<int[]>> operatorCombine(int count) {
 		Map<Integer, List<int[]>> combinations = new HashMap<>();
 		ICombinatoricsVector<Integer> operatorsVectors = Factory
@@ -39,14 +39,56 @@ class Combinator {
 			List<ICombinatoricsVector<Integer>> gens = gen.generateAllObjects();
 			System.out.println("Permutations za " + operatorCount + " ni "
 					+ gens.size());
+			List<int[]> arrangedInts = new ArrayList<>();
 			while (it.hasNext()) {
-				for (Integer i : it.next()) {
+				int incrementor = 0;
+				ICombinatoricsVector<Integer> nextItaration = it.next();
+				int[] ints = new int[nextItaration.getSize()];
+				for (Integer i : nextItaration) {
 					System.out.print(i.intValue() + " ");
+					ints[incrementor] = i;
+					++incrementor;
 				}
-				System.out.println();
+				System.out.println("==========================" + ints.length);
+				arrangedInts.add(ints);
 			}
+			combinations.put(operatorCount, arrangedInts);
 		}
-		return null;
+		return combinations;
+	}
+	
+	// Permutations without repetition (number combinations)
+	private List<Integer[]> numberPermutations(Integer[] combination){
+		List<Integer[]> combinations = new ArrayList<>();
+		ICombinatoricsVector<Integer> permutationsVector = Factory.createVector(combination);
+		Generator<Integer> gen = Factory.createPermutationGenerator(permutationsVector);
+		Iterator<ICombinatoricsVector<Integer>> it = gen.iterator();
+		while(it.hasNext()){
+			int incrementor = 0;
+			ICombinatoricsVector<Integer> nextIteration = it.next();
+			Integer[] ints = new Integer[nextIteration.getSize()];
+			for (Integer i : nextIteration) {
+				ints[incrementor] = i;
+				++incrementor;
+			}
+			combinations.add(ints);
+		}
+		
+		return combinations;
 	}
 
+	//Number combinations permuted
+	public Map<Integer, List<Integer[]>> numberCombinationsPermuted(Map<Integer, List<Integer[]>> numberCombinations){
+		Map<Integer, List<Integer[]>> finalPermutations = new HashMap<>();
+		for(Integer i : numberCombinations.keySet()){
+			List<Integer[]> mapRowCombinations = numberCombinations.get(i);
+			List<Integer[]> finalMapRowCombinations = new ArrayList<>();
+			for (Integer[] js : mapRowCombinations) {
+				List<Integer[]> permutedCombs = numberPermutations(js);
+				finalMapRowCombinations.addAll(permutedCombs);
+			}
+			finalPermutations.put(i, finalMapRowCombinations);
+		}
+		return finalPermutations;
+	}
 }
--- Combinator/src/TestIt.java
+++ Combinator/src/TestIt.java
@@ -6,7 +6,6 @@ public class TestIt {
 	public static void main(String[] args) {
 		Combinator comb = new Combinator();
 		Map<Integer, List<int[]>> combinations = comb.numberCombine(6);
-		System.out.println(combinations.size());
 		for (int i = 0; i < combinations.size(); i++) { // To get to map row
 			for (int j = 0; j < combinations.get(i).size(); j++) { // To get to
 																	// list in
